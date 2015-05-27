1c55294092177891f295396aa3470e1a00ce6fb0
Ryan Mathews <ryan@ryanjm.com>
2015-05-27 09:14:13 -0600

    Added the textview to a container view
    Can't get it to work.

--- autolayout_demo_2/TextViewTVCell.m
+++ autolayout_demo_2/TextViewTVCell.m
@@ -12,21 +12,21 @@
   CGFloat _textViewHeight;
 }
 
-- (void)updateConstraints
-{
-//    self.textViewWidthConstraint.constant = self.contentView.frame.size.width - 30;
-    self.containerViewWidthConstraint.constant = CGRectGetWidth(self.contentView.frame);
-    [super updateConstraints];
-}
+//- (void)updateConstraints
+//{
+////    self.textViewWidthConstraint.constant = self.contentView.frame.size.width - 30;
+////    self.containerViewWidthConstraint.constant = CGRectGetWidth(self.contentView.frame);
+//    [super updateConstraints];
+//}
 
 - (void)textViewDidChange:(UITextView *)textView {
     CGFloat intrinsicHeight = [textView intrinsicContentSize].height;
-    if ([textView.text length] > 150) {
-        self.textViewWidthConstraint.constant = 80;
-    }
-    else {
-        self.textViewWidthConstraint.constant = 40;
-    }
+//    if ([textView.text length] > 150) {
+//        self.textViewWidthConstraint.constant = 80;
+//    }
+//    else {
+//        self.textViewWidthConstraint.constant = 40;
+//    }
     if (_textViewHeight != 0.f && _textViewHeight != intrinsicHeight) {
         NSLog(@"\ncell bounds: %@\nintrinsic size of textview: %@\nintrinsic size of cell: %@", NSStringFromCGRect(self.frame), NSStringFromCGSize([textView intrinsicContentSize]), NSStringFromCGSize([self.contentView intrinsicContentSize]));
         [[UIApplication sharedApplication] sendAction:@selector(respondToTextViewHeightDidChange:) to:nil from:self forEvent:nil];
